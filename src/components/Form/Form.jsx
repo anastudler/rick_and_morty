32fea7bf0bfcc697e5fa4b5de827301c301f1a0b
Ana Studler <anistudler@gmail.com>
2023-03-03 11:19:34 -0300

    Cree carpeta Detail, Form, Nav y SearchBar

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,15 @@
+const Form = () => {
+    return (
+        <form action="">
+            <label htmlFor="username">Username:</label>
+            <input type="text" name="username" />
+
+            <label htmlFor="password">Password:</label>
+            <input type="password" name="password" id="" />
+
+            <button>LOGIN</button>
+        </form>
+    )
+}
+
+export default Form;
